46541fdd359883ef868115649a760da18429af40
dautiep <dautiep123@gmail.com>
2023-04-06 18:25:12 +0700

    somthings about HTML DOM

---
+++ HTML_DOM/main.js
@@ -0,0 +1,23 @@
+/**
+ * HTML DOM: Document Object Model
+ * 
+ * - Khi load một trang web thì trình duyệt web sẽ tạo ra một model DOM.
+ * - DOM có 3 thành phần: element, attribute, text
+ * 
+ *      + Tag (thẻ HTML) là element
+ *      + Thuộc tính của thẻ mở HTML là attribute
+ *      + Chữ trong html (text) là text
+ * 
+ * - Node là một ô vuông trong HTML DOM
+ * 
+ * ------------------------------------------------
+ * Phân biệt HTML DOM và javascript
+ * 
+ * - Javascript chỉ cung cấp công cụ truy xuất vào HTML DOM.
+ * - Javascript có thể chạy được ở: Browser | Server (NodeJS)
+ * Browser: HTML -> DOM -> WEB API (location. notification khi load web)
+ * 
+ */
+
+//Phương thức nhận vào là một chuỗi
+document.write('HELLO GUYS');
